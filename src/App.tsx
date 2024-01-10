a1fa624a393077ce487c986bb099171f7d56e656
Mykola Plekan <zikzar@gmail.com>
2024-01-10 20:38:27 +0200

    phone catalog

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import './App.scss';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { HomePage } from './pages/HomePage';
+import { PhonesPage } from './pages/PhonesPage';
+import { ProductDetailsPage } from './pages/ProductDetailsPage';
+import { FavouritesPage } from './pages/FavouritesPage';
+import { CartPage } from './pages/CartPage';
+import { TabletsPage } from './pages/TabletsPage';
+import { AccessoriesPage } from './pages/AccessoriesPage';
+import { PhoneNotFoundPage } from './pages/PhoneNotFoundPage';
+import { NotFoundPage } from './pages/NotFoundPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/phones">
+        <Route index element={<PhonesPage />} />
+        <Route path=":productId" element={<ProductDetailsPage />} />
+        <Route path="not-found" element={<PhoneNotFoundPage />} />
+      </Route>
+      <Route path="/tablets" element={<TabletsPage />} />
+      <Route path="/accessories" element={<AccessoriesPage />} />
+      <Route path="/favourites" element={<FavouritesPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+
+    <Footer />
+  </div>
+);
 
 export default App;
---
+++ src/pages/TabletsPage.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable max-len */
+export const TabletsPage = () => {
+  return (
+    <div className="cart">
+      <h1 className="cart__title">We are sorry, but this page is not implemented yet</h1>
+    </div>
+  );
+};
